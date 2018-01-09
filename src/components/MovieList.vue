42df0c1bd80e9384ca80b368bfa397e326c19900
Liz Maple <liz@uk.ibm.com>
2018-01-09 16:04:00 +0000

    First commit - added in separate .vue files for each component

---
+++ src/components/MovieList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="movie-list">
+    <movie-item v-for="movie in filteredMovies" class="movie" v-bind:movie="movie"></movie-item>
+  </div>
+</template>
+
+<script>
+import genres from '../util/genres';
+import MovieItem from './MovieItem.vue';
+
+export default {
+    props: ['genre', 'time', 'movies'],
+    methods: {
+      moviePassesGenreFilter(movie) {
+        if (!this.genre.length){
+          return true;
+        } else {
+          return this.genre.find(genre => movie.genre === genre);
+        }
+      }
+    },
+    computed: {
+      filteredMovies() {
+        return this.movies.filter(this.moviePassesGenreFilter);
+      }
+    },
+    components: {
+      MovieItem
+    }
+  }
+</script>
