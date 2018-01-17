21d43d723ebd94b24311018deda4774536770438
Liz Maple <liz@uk.ibm.com>
2018-01-17 12:22:55 +0000

    Final update of project - finished!

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import moment from 'moment-timezone';
 moment.tz.setDefault("UTC");
 Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.moment} });
 
-import { checkFilter } from './util/bus';
+import { checkFilter, setDay } from './util/bus';
 const bus = new Vue();
 Object.defineProperty(Vue.prototype, '$bus', { get() {return this.$root.bus} });
 
@@ -18,6 +18,9 @@ Vue.use(VueRouter);
 
 import routes from './util/routes';
 const router = new VueRouter({ routes });
+
+import Tooltip from './util/tooltip';
+Vue.use(Tooltip);
 
 new Vue({
   el: '#app',
@@ -34,6 +37,7 @@ new Vue({
       this.movies = response.data;
     });
     this.$bus.$on('check-filter', checkFilter.bind(this));
+    this.$bus.$on('set-day', setDay.bind(this));
   },
   router
 });
